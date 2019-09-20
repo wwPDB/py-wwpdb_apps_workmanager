d798bd6afd3660a742dcd342a7d891f72314a8ce
Ezra Peisach <ezra.peisach@rcsb.org>
2019-09-20 06:14:43 -0400

    0.6.dev3 fix for SeqModUI not working

--- wwpdb/apps/workmanager/depict/DepictBase.py
+++ wwpdb/apps/workmanager/depict/DepictBase.py
@@ -294,7 +294,8 @@ class DepictBase(object):
         """
         """
         mod_url_list = [ [ 'AnnMod', 'SITE_ANN_TASKS_URL' ], [ 'LigMod', 'SITE_LE_URL' ], [ 'LigModUI', 'SITE_LE_URL' ], \
-                         [ 'SeqMod', 'SITE_SE_URL' ], [ 'TransMod', 'SITE_TRANS_EDITOR_URL' ], [ 'ValMod', 'SITE_VAL_TASKS_URL' ] ]
+                         [ 'SeqMod', 'SITE_SE_URL' ], [ 'SeqModUI', 'SITE_SE_URL' ], [ 'TransMod', 'SITE_TRANS_EDITOR_URL' ], \
+                         [ 'ValMod', 'SITE_VAL_TASKS_URL' ] ]
         #
         cI = ConfigInfo(self._siteId)
         for list in mod_url_list:
